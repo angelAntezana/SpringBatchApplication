c4764b11130db0c11b55524b61aba710467f1bb5
angelAntezana <angel2estevan@gmail.com>
2025-01-12 21:24:59 +0100

    Finish decompress step

--- src/main/java/com/batch/config/BatchConfiguration.java
+++ src/main/java/com/batch/config/BatchConfiguration.java
@@ -1,12 +1,19 @@
 package com.batch.config;
 
+import com.batch.steps.ItemDecompressStep;
 import com.batch.steps.ItemProcessorStep;
 import com.batch.steps.ItemReaderStep;
+import com.batch.steps.ItemWriterStep;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BatchConfiguration {
+
+    @Bean
+    public ItemDecompressStep itemDecompressStep() {
+        return new ItemDecompressStep();
+    }
 
     @Bean
     public ItemReaderStep itemReaderStep() {
@@ -16,5 +23,10 @@ public class BatchConfiguration {
     @Bean
     public ItemProcessorStep itemProcessorStep() {
          return new ItemProcessorStep();
+    }
+
+    @Bean
+    public ItemWriterStep itemWriterStep() {
+        return  new ItemWriterStep();
     }
 }
